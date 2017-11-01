67d4c4e2eb810780b9b3e539f7aac0e9adf7c5a8
DukeDesign <aaronduke22@gmail.com>
2017-11-01 15:30:59 +0000

    test

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$('a').click(function(evt) {
+   event.stopPropagation();
+});
+
+$('a[href="#"]').click(function(evt) {
+   event.preventDefault();
+});;
+
+$("#home").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".banner").offset().top
+    }, 2000);
+});
+
+$("#menu").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".main").offset().top
+    }, 2000);
+});
+
+$("#gallery").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".photos").offset().top
+    }, 2000);
+});
+
+$("#info").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".contact").offset().top
+    }, 2000);
+});
+
+$(".menuButton").click(function() {
+    $("#mobileNav").toggleClass("view");
+});
